4c50aba6328946298265d20100b53c6034319995
Majid <mj_azani@hotmail.com>
2014-02-03 14:04:51 +0330

    plugin info updated

--- changelog.php
+++ changelog.php
@@ -10,10 +10,10 @@
  * @wordpress-plugin
  * Plugin Name:       Changelog
  * Plugin URI:        https://github.com/M4Gd/changelog
- * Description:       A plugin to add and display theme and plugins changelogs
+ * Description:       A WordPress plugin that allows you to add, archive and display your product's changelog in your WordPress website.
  * Version:           1.0.1
  * Author:            averta
- * Author URI:        https://github.com/M4Gd/
+ * Author URI:        http://averta.net
  * Text Domain:       changelog
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt

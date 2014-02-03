57fa6d38559943ce3023e92f41ae468a4d14df5d
Majid <mj_azani@hotmail.com>
2014-02-03 12:37:28 +0330

    date picker and input mask added to custom metaboxes

--- admin/assets/js/admin.js
+++ admin/assets/js/admin.js
@@ -3,8 +3,13 @@
 
 	$(function () {
 
-		// Place your administration-specific JavaScript here
+		// init date picker for changelog release date
+		$('#release_date').datepicker({
+            dateFormat: "dd.mm.yy",
+            showButtonPanel: true
+        });
 
+		$('#release_version').mask("9.9.9");
 	});
 
 }(jQuery));
